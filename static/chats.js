9216a93bc29456911b25f094bf9d5853f28bc7e1
janispurins64 <janispurins64@gmail.com>
2020-01-10 16:07:02 +0200

    daudz

--- static/chats.js
+++ static/chats.js
@@ -1,1 +1,46 @@
-console.log("Sveciens noaa")
+const ATJAUNOT = 5000;
+
+async function lasiChatu() {
+    const atbilde = await fetch('/chats/lasi');
+    const datuObjekts = await atbilde.json();
+    raadiChatuVienkarsi(datuObjekts);
+    await new Promise(resolve => setTimeout(resolve, ATJAUNOT));
+    await lasiChatu();
+}
+
+function raadiChatuVienkarsi(dati) {
+    let jaunaRinda = "<br/>";
+    let chats = "";
+    let chataDiv = document.getElementById("chats");
+    for (let rinda of dati["chats"]) {
+        chats = chats + rinda + jaunaRinda;
+    }
+    chataDiv.innerHTML = chats;
+}
+
+async function suutiZinju() {
+   // Nolasa ievades lauka saturu
+    let zinjasElements = document.getElementById("zinja");
+    let zinja = zinjasElements.value;
+    // izdzēš ievades lauku
+    zinjasElements.value = "";
+    const atbilde = await fetch('/chats/suuti', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ "chats": zinja })
+    });
+    const datuObjekts = await atbilde.json();
+    // parāda jauno chata saturu
+    raadiChatuVienkarsi(datuObjekts);
+
+    let ievadesLauks = document.getElementById("zinja");
+
+    ievadesLauks.addEventListener("keyup", function(event){
+        if(event.keyCode === 13){
+            suutiZinju();
+        }
+    })
+}
+
