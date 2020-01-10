5684ca035ef4506b468cbe6c32fb9b5203b66575
janispurins64 <janispurins64@gmail.com>
2020-01-10 14:39:15 +0200

    zinja

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, json, jsonify, request
 
 app = Flask('app')
 
@@ -9,7 +9,22 @@ def index_page():
 
 @app.route('/help')
 def healt_check():
-      return "OK"
+  return "OK Viss kartibaa"
+
+@app.route('/chats/lasi')
+def ielasit_chatu():
+  chata_rindas = []
+  with open("chats.txt","r",encoding="UTF-8") as f:
+    for rinda in f:
+          chata_rindas.append(rinda)
+  return jsonify({"chats":chata_rindas})
+
+@app.route('/chats/suuti',methods=["POST"])
+def suutiit_zinju():
+  dati = request.json    
+  with open("chats.txt","a",newline="") as f:
+    f.write(dati['chats']+"\n")
+  return ielasit_chatu()
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
