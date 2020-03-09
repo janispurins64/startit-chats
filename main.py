c5e15c5750348b92dbd828abd9e2cdc1871999a6
janispurins64 <janispurins64@gmail.com>
2020-03-09 21:32:32 +0200

    Pievienota datubāze
    PostgreSQL tests pievienots

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
+import os
 from flask import Flask, render_template, json, jsonify, request
-
+from flask_sqlalchemy import SQLAlchemy
 app = Flask('app')
+app.config['SQLALCHEMY_DATABASE_URI']=os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
 
+class test(db.Model):
+      col = db.Column(db.String(255), primary_key=True)
+      col2 = db.Column(db.String(255),unique=True, nullable=False)
+
+      def __repr__(self):
+            return '%r' % self.col
+
+@app.route('/postgreSQL')
+def postgresSQL():
+  result = test.query.all()
+  return '%r' % result
 
 @app.route('/')
 def index_page():
